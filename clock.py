6d79a4f2267ea88a03f2abb3cdbdf9cba5a95e2a
tonghs <tonghuashuai@gmail.com>
2020-10-19 15:14:53 +0800

    ws2812 clock

---
+++ clock.py
@@ -0,0 +1,68 @@
+from time import sleep
+import datetime
+from rpi_ws281x import PixelStrip, Color
+from libs.ws281.utils import base_bmp, bmplist2matrix, show, render_char
+
+
+COLUMN = 32
+ROW = 8
+
+# LED strip configuration:
+LED_COUNT = ROW * COLUMN  # Number of LED pixels.
+LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10  # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 4  # Set to 0 for darkest and 255 for brightest
+# True to invert the signal (when using NPN transistor level shift)
+LED_INVERT = False
+LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+# Create NeoPixel object with appropriate configuration.
+strip = PixelStrip(
+    LED_COUNT,
+    LED_PIN,
+    LED_FREQ_HZ,
+    LED_DMA,
+    LED_INVERT,
+    LED_BRIGHTNESS,
+    LED_CHANNEL,
+)
+
+strip.begin()
+
+
+def show_time():
+
+    # 生成 8 行 32 列的 matrix，对应
+    _base_bmp = base_bmp * LED_COUNT
+    base_matrix = bmplist2matrix(_base_bmp, COLUMN, ROW)
+
+    x = 2
+    y = 1
+    char_list = []
+
+    for i, char in enumerate(datetime.datetime.now().strftime('%H:%M:%S')):
+        char_list.append((x, y, char))
+        if i % 3:
+            x += 3
+        else:
+            x += 4
+
+    render_char(base_matrix, char_list)
+    show(strip, base_matrix)
+
+
+def main():
+    while True:
+        show_time()
+        sleep(1)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        for i in range(LED_COUNT):
+            strip.setPixelColor(i, Color(0, 0, 0))
+        strip.show()
---
+++ libs/ws281/utils.py
@@ -0,0 +1,81 @@
+from rpi_ws281x import Color
+from .matrix_dict import char2matrix
+
+base_bmp = [0x00000000]  # 一个像素点
+
+
+def xy_to_array_index(x, y):
+    if x % 2 == 0:
+        return (x * 8) + y
+    else:
+        return (x * 8) + (8 - y - 1)
+
+
+def bmplist2matrix(bmp, width, height):
+    """
+    将 bmp list 转换为 width 宽 height 高的 matrix
+    并将 hex 的颜色转为 10 进制 rgb
+    """
+    matrix = []
+    for y in range(height):
+        row = []
+        for x in range(width):
+            index = x + (y * width)
+            color = bmp[index]
+            color_r = color >> 24 & 0xFF
+            color_g = color >> 16 & 0xFF
+            color_b = color >> 8 & 0xFF
+            row.append((color_r, color_g, color_b))
+        matrix.append(row)
+
+    return matrix
+
+
+def replace_matrix(base, x, y, data):
+    """
+    base = [[1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]]
+    x = 1
+    y = 0
+    data = [[x],
+            [y],
+            [z]]
+
+    return [[1, x, 3],
+            [4, y, 6],
+            [7, z, 9]]
+    """
+
+    data_height = len(data)
+    data_width = len(data[0])
+
+    for _row in range(data_height):
+        for _col in range(data_width):
+            base[y + _row][x + _col] = data[_row][_col]
+
+    return base
+
+
+def render_char(base_matrix, char_list):
+    """
+    在 base_matrix 上渲染需要的字符
+
+    base_matrix: 生成的 width * height 匹配灯珠阵列的矩阵
+    char_list: 要渲染的字符: [(x, y, char)] demo: [(0, 0, 1), (2, 3, 'x')]
+    """
+    for x, y, char in char_list:
+        bmp_data, width, height = char2matrix(char)
+        bmp_data_matrix = bmplist2matrix(bmp_data, width, height)
+        replace_matrix(base_matrix, x, y, bmp_data_matrix)
+
+
+def show(strip, matrix):
+    width = len(matrix[0])
+    height = len(matrix)
+    for row in range(height):
+        for col in range(width):
+            color_r, color_g, color_b = matrix[row][col]
+            strip.setPixelColor(xy_to_array_index(col, row), Color(color_r, color_g, color_b))
+
+    strip.show()
